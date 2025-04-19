df48187d45aacb632447cf691c3602404025d296
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-04-19 12:36:52 +0300

    express setup done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
 import express from "express";
 const app = express();
-const port = 3000;
+const port = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Howdy!");
+});
+
+app.get('/health', (req, res) => {
+    res.send('OK');
+});
+
+app.get("/calculator/:id", (req, res) => {
+    console.log(`Query parameter: ${JSON.stringify(req.query)}`);
+    console.log(`Headers: ${JSON.stringify(req.headers)}`);
+    console.log(`Method: ${req.method}`);
+    res.status(202).header({'X-Custom-Header': 'foo', 'X-Custom-Header-2': 'fuu'}).send(`Hello ${req.params.id}`);
+});
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
