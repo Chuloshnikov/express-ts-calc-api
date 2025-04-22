e5cc15278c0ff14b597f40fcd31da6f06222e085
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-04-22 10:06:29 +0300

    done

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,12 @@
 import express from "express";
+import { healthRouter, calculatorRouter } from './routes'
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
-    res.send("Howdy!");
-});
 
-app.get('/health', (req, res) => {
-    res.send('OK');
-});
-
-app.get("/calculator/:id", (req, res) => {
-    console.log(`Query parameter: ${JSON.stringify(req.query)}`);
-    console.log(`Headers: ${JSON.stringify(req.headers)}`);
-    console.log(`Method: ${req.method}`);
-    res.status(202).header({'X-Custom-Header': 'foo', 'X-Custom-Header-2': 'fuu'}).send(`Hello ${req.params.id}`);
-});
-
-
+// routes
+app.use('/health', healthRouter);
+app.use('/calculator', calculatorRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
